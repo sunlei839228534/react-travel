d2d358baeefc95de9e6e7287ad0c7ceb833d1532
孙磊 <839228534@qq.com>
2023-05-21 12:36:48 +0800

    feat: 完成函数式组建的语言切换

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,11 +1,15 @@
 import { Layout, Typography } from "antd";
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 export const Footer: React.FC = () => {
+
+  const { t } = useTranslation()
+
   return (
     <Layout.Footer>
       <Typography.Title level={3} style={{ textAlign: 'center' }}>
-        版权所有 @ React 旅游网
+        {t("footer.detail")}
       </Typography.Title>
     </Layout.Footer >
   )
